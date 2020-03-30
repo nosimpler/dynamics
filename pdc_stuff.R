315cdb41e4cfab4f1ec2cb47233bf980a3f7d0d9
Rob Law <rlaw1@bwh.harvard.edu>
2020-03-30 09:36:09 -0400

    many new distance measures for hypnogram

--- pdc_stuff.R
+++ pdc_stuff.R
@@ -6,14 +6,14 @@
 IDlist <- flatten(tibble(ID=intersect(unique(m2$ID), unique(m1$ID))))
 
 # select band/channel
-chb <- 'DELTA_C3'
+chb <- 'DELTA_F4'
 
 # m time-delays, t steps back
-m=5
+m=4
 t=1
 
 # use n epochs
-n_e <- 420
+n_e <- 100
 
 
 dc3_1 <- m1c %>% recast() %>%
@@ -36,11 +36,21 @@ dc3_2$E_SLEEP <- rep(seq(1:n_e), times=405)
 dc3w_1 <- pivot_wider(dc3_1, names_from=ID, values_from=!!sym(chb))
 dc3w_2 <- pivot_wider(dc3_2, names_from=ID, values_from=!!sym(chb))
 
-pdc_dist1 <- pdcDist(dc3w_1[2:n_e,],m=m, t=t)
-pdc_dist2 <- pdcDist(dc3w_2[2:n_e,],m=m, t=t)
+um01 <- umap(t(dc3w_1[,2:406]))
+um02 <- umap(t(dc3w_2[,2:406]))
 
-p_matrix1 <- matrix(pdc_dist1, nrow=length(pdc_dist1)/405, ncol=405)
-p_matrix2 <- matrix(pdc_dist2, nrow=length(pdc_dist2)/405, ncol=405)
+plot(um01$layout[,1], um01$layout[,2])
+plot(um02$layout[,1], um02$layout[,2])
+
+pdc_dist1 <- pdcDist(dc3w_1[,2:406],m=m, t=t)
+pdc_dist2 <- pdcDist(dc3w_2[,2:406],m=m, t=t)
+
+
+p_matrix1 <- matrix(0,405, 405)
+p_matrix1[lower.tri(p_matrix1, diag=FALSE)] <- pdc_dist1
+
+p_matrix2 <- matrix(0,405, 405)
+p_matrix2[lower.tri(p_matrix2, diag=FALSE)] <- pdc_dist2
 
 um1 <- umap(p_matrix1)
 um2 <- umap(p_matrix2)
@@ -52,5 +62,7 @@ nights_umap <- tibble(N1C1=um1$layout[,1],
                       N1C2=um1$layout[,2],
                       N2C1=um2$layout[,1],
                       N2C2=um2$layout[,2])
-ggplot(nights_umap,aes(x=N1C1, y=N1C2, color=N2C2))+geom_point()
+ggplot(nights_umap,aes(x=N1C1, y=N1C2, color=N2C2))+
+  geom_point()+
+  scale_color_distiller(palette='Spectral')
 
